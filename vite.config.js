24db937c0e73b4ea8412a6e0a79ce49ad58e398f
Christian Yarwood <christianyarwood@gmail.com>
2025-11-08 11:37:11 +0000

    Update base to vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/AI-Productivity-Planner/",
   plugins: [react(), tailwindcss()],
 });
